00199cea71f7063c7d29fff31669907f594115a0
fl_3650 <repilovi@gmail.com>
2024-02-11 15:09:43 +0300

    In this branch we begin to fully switch to MVC. The View class has been added, ALL SWING ELEMENTS MUST BE IN IT. The Controller class has been added from which the Model classes (com.rgbteam.chemistry) will be secretly called. Entry point of the program is the Main class from which the View constructor is called. For now, the View constructor is just a renamed main from which the model is being directly called

---
+++ src/main/java/com/rgbteam/Controller.java
@@ -0,0 +1,21 @@
+package com.rgbteam;
+
+import com.rgbteam.chemistry.Element;
+
+public class Controller {
+    public Element retrieveElementByNumber() {
+        return null;
+    }
+
+    public Element retrieveElementByShortName() {
+        return null;
+    }
+
+    public Element retrieveElementByFullName() {
+        return null;
+    }
+
+    public double calculateCompoundsAtomicMass() {
+        return 0f;
+    }
+}
