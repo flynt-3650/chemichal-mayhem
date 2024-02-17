3b9dc5505cedf98fce86097a6d73300733d4321a
Lisa <elbondarenko04@gmail.com>
2024-02-17 14:13:24 +0300

    done Controller and integrate Controller in View

--- src/main/java/com/rgbteam/Controller.java
+++ src/main/java/com/rgbteam/Controller.java
@@ -1,25 +1,32 @@
 package com.rgbteam;
 
+import com.rgbteam.chemistry.Compound;
 import com.rgbteam.chemistry.Element;
+import com.rgbteam.chemistry.PeriodicTable;
 
 public class Controller {
+
     public Element retrieveElementByNumber(int number) {
         // ToDo: call getElementByNumber from PeriodicTable here & return Element
-        return null;
+        return PeriodicTable.getElementByNumber(number);
     }
 
     public Element retrieveElementByShortName(String shortName) {
         // ToDo: call getElementByShortName from PeriodicTable here & return Element
-        return null;
+        return PeriodicTable.getElementByShortName(shortName);
+
     }
 
     public Element retrieveElementByFullName(String fullName) {
         // ToDo: call getElementByFullName from PeriodicTable here & return Element
-        return null;
+        return PeriodicTable.getElementByFullName(fullName);
     }
 
     public double calculateCompoundsAtomicMass(String rawCompound) {
         // ToDo: call calculateAtomicMass from Compound here & return value
-        return 0f;
+        Compound compound = new Compound(rawCompound);
+        return compound.calculateAtomicMass();
     }
+
+
 }
